696748e16e4445141f118923e0be9cf152845d6f
Dan Stronge <dnlstronge@gmail.com>
2023-02-03 11:06:47 +0000

    [x] - add conditional for LCW element

--- src/components/UC/UCform/PA/LCW.js
+++ src/components/UC/UCform/PA/LCW.js
@@ -6,10 +6,20 @@ const UC_elements = UCElements
 
 const LCW = ({ lift_state, set_lift_state, PA_state, set_PA_state }) => {
 
-
+   const LCW_LCWRA_handler = (e) => {
+        if(e.target.value === "NO") {
+            set_lift_state({...lift_state, LCW_LCWRA: e.target.value})
+        }
+        if(e.target.value === "YES") {
+            set_lift_state({...lift_state, LCW_LCWRA: e.target.value})
+        }
+   }
    const LCW_handler = (e) => {
         if(e.target.value === "YES") {
-
+            set_PA_state({PA_state, LCW: UC_elements.LCW})
+        }
+        if(e.target.value === "NO") {
+            set_PA_state({PA_state, LCW: 0})
         }
     }
 
@@ -17,16 +27,23 @@ const LCW = ({ lift_state, set_lift_state, PA_state, set_PA_state }) => {
     return (
         <div className={classes.LCW_container}>
         <label className={classes.LCW_label} htmlFor="LCW_LCWRA"></label>
-        <select className={classes.LCW_select} id="LCW_LCWRA"></select>
-        <h4>For ESA migration case with ESA LCW in payment</h4>
-            <label className={LCW_label} htmlFor="selectLCW">Add LCW to claim 
-            <select className{LCW_select} id="selectLCW">
-                <option value="NONE">--select--</option>
+        Does the claim include LCW or LCWRA
+        <select onChange={LCW_LCWRA_handler}className={classes.LCW_select} id="LCW_LCWRA">
+            <option value="NO">--select--</option>
+            <option value="LCW">LCW only</option>
+            <option value="LCWRA">LCWWRA</option>
+            <option value="NO">Neither</option>
+        </select>
+       
+       {lift_state.LCW_LCWRA === "LCW" && 
+            <label className={classes.LCW_label} htmlFor="selectLCW">Add LCW to claim 
+            <select onChange={LCW_handler} className={classes.LCW_select} id="selectLCW">
+                <option value="NO">--select--</option>
                 <option value="YES (LCW element payable)">Yes</option>
                 <option value="NO (has LCW status only)">No</option>
-                <option value="NONE"></option>
+                
             </select>
-            </label>
+            </label>}
             
             
         </div>
