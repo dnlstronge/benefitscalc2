b34dbe9f41a9e15ca80bd4f48a7d28b08c261379
Dan Stronge <dnlstronge@gmail.com>
2023-02-04 10:08:00 +0000

    [x] add handler

--- src/components/UC/UCform/PA/LCWRA.js
+++ src/components/UC/UCform/PA/LCWRA.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import classes from "./LCWRA.module.css";
 
 const LCWRA = ( {set_lift_state, PA_state, set_PA_state }) => {
+    const handle_single = (e) => {
+            
+    }
     return (
 
         <div className={classes.container}>
-            <label htmlFor="single_label">Add LCWRA component
-                <select>
-                    <option value="NONE"></option>
-                    <option value="YES"></option>
-                    <option value="NONE"></option>
+            <label className={classes.LCWRA_label} htmlFor="single_label">Add LCWRA component
+                <select className={classes.LCWRA_select} id="single_label">
+                    <option value="NONE">--select</option>
+                    <option value="YES">Yes</option>
+                    <option value="NONE">No</option>
                 </select>
             </label>
         </div>
