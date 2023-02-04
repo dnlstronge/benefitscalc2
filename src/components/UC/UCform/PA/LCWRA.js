1c8f6d69ab6bb8370fbe891b1df86fa0e73d0b72
Dan Stronge <dnlstronge@gmail.com>
2023-02-04 10:11:07 +0000

    [x] - hook up handler [x] - import dynamic values from UC elements

--- src/components/UC/UCform/PA/LCWRA.js
+++ src/components/UC/UCform/PA/LCWRA.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import classes from "./LCWRA.module.css";
+import UCElements from "../../UCElements/UCElements";
+
+
+const UC_elements = UCElements
 
 const LCWRA = ( {set_lift_state, PA_state, set_PA_state }) => {
     const handle_single = (e) => {
-            
+            if(e.target.value === "NONE") {
+                set_PA_state({...PA_state, LCWRA: 0})
+            }
     }
     return (
 
         <div className={classes.container}>
             <label className={classes.LCWRA_label} htmlFor="single_label">Add LCWRA component
-                <select className={classes.LCWRA_select} id="single_label">
+                <select onChange={handle_single} className={classes.LCWRA_select} id="single_label">
                     <option value="NONE">--select</option>
                     <option value="YES">Yes</option>
                     <option value="NONE">No</option>
