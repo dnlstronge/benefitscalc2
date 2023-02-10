5ee4939df1f67162349b1bf41bd9b6917189c150
Dan Stronge <dnlstronge@gmail.com>
2023-02-10 16:57:52 +0000

    [x] set up drop down to update local state [] hook up useEffect to watch for changes and pass     dispatch action

--- src/components/UC2/WCA/Transitional.js
+++ src/components/UC2/WCA/Transitional.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Transitional.module.css"
 import UCElements from "../UCElements/UCElements";
 
@@ -8,17 +8,25 @@ const transB = UC_elements.TA_2
 const transC = UC_elements.TA_3
 
 const Transitional = (props) => {
+
+    const [trans, setTrans] = useState(0)
+
+
+    const handleTrans = (e) => {
+        e.target.value && setTrans(e.target.value)
+    }
     return (
         <div className={classes.container}>
             <label className={classes.TA_label} htmlFor="transitional_amounts"></label> Transiton from SDP legacy
-                <select id="transitional_amount">
-                    <option>--select</option>
-                    <option>No</option>
-                    <option>£{transA}</option>
-                    <option>£{transB}</option>
-                    <option>£{transC}</option>
+                <select className={classes.TA_select} id="transitional_amount">
+                    <option value={0}>--select</option>
+                    <option value={0} >No</option>
+                    <option value={transA}>£{transA}</option>
+                    <option value={transB}>£{transB}</option>
+                    <option value={transC}>£{transC}</option>
                     
                 </select>
+                <div>TEST: {trans} </div>
         </div>
     )
 }
