58e32cba86107168d00139ba3eba38a752fbfd8c
Dan Stronge <dnlstronge@gmail.com>
2023-02-11 14:34:42 +0000

    [x] - add logic to calculation

--- src/components/UC2/CHILD/CHILD.js
+++ src/components/UC2/CHILD/CHILD.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import classes from "./CHILD.module.css"
 import UCElements from "../UCElements/UCElements"
 
-const preARIL = UCElements.child_1st
-const postARIL = UCElements.child_2nd
+const preAPRIL = UCElements.child_1st
+const postAPRIL = UCElements.child_2nd
 
 const CHILD = ({ setPropState }) => {
 
@@ -15,16 +15,24 @@ const CHILD = ({ setPropState }) => {
           
     }
     const calcChildren = (n) => {
-        if(oldest && n == 1) {
-            return preARIL 
+        
+        if(oldest === true && n === 1) {
+            return preAPRIL 
         }
-        if(oldest && n > 1 ) {
-            return (n * postARIL - postARIL) + preARIL
+        if(oldest === true && n > 1 ) {
+            return (n * postAPRIL - postAPRIL) + preAPRIL
+        }
+        else {
+            return n * postAPRIL
         }
     }
 
     const handleChildren = (e) => {
-        
+        if(e.target.value === 0 ) {
+            return
+        } else {
+            return  setNumChildren(calcChildren(e.target.value))
+        }
     }
     return (
         <React.Fragment>
@@ -39,7 +47,7 @@ const CHILD = ({ setPropState }) => {
           {/* oldest born after */}
           <div className={classes.sub_container}>
             <label className={classes.child_label} htmlFor="children_selection"> Number of Children exempt from 2 child limit
-                <select className={classes.child_select} id="children_selection">
+                <select onChange={handleChildren} className={classes.child_select} id="children_selection">
                     <option value="0">--select--</option>
                     <option value="0">none</option>
                     <option value="1">1</option>
@@ -54,6 +62,7 @@ const CHILD = ({ setPropState }) => {
                     <option value="10">10</option>
                 </select>
             </label>
+            <div>TEST {numChildren} </div>
           </div>
 
             
