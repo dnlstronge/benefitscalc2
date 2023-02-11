c92c41d91ef4f4efdc447b895d0d58da1c35c779
Dan Stronge <dnlstronge@gmail.com>
2023-02-11 14:19:54 +0000

    [x] - create equation to solve chidren problem

--- src/components/UC2/CHILD/CHILD.js
+++ src/components/UC2/CHILD/CHILD.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import classes from "./CHILD.module.css"
+import UCElements from "../UCElements/UCElements"
+
+const preARIL = UCElements.child_1st
+const postARIL = UCElements.child_2nd
 
 const CHILD = ({ setPropState }) => {
 
@@ -10,6 +14,14 @@ const CHILD = ({ setPropState }) => {
           
           
     }
+    const calcChildren = (n) => {
+        if(oldest && n === 1) {
+            return preARIL 
+        }
+        if(oldest && n > 1 ) {
+            return (n * postARIL - postARIL) + preARIL
+        }
+    }
     return (
         <React.Fragment>
             <div className={classes.container}>
@@ -19,8 +31,26 @@ const CHILD = ({ setPropState }) => {
                 </label>
                 <label className={classes.dropdown_label} htmlFor="exempt_child"></label>
             </div>
-            {oldest && 
-            <div>TEST BABY!!! </div>}
+
+          {/* oldest born after */}
+          <div className={classes.sub_container}>
+            <label className={classes.child_label} htmlFor="children_selection"> Number of Children exempt from 2 child limit
+                <select className={classes.child_select} id="children_selection">
+                    <option>--select--</option>
+                    <option>none</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                </select>
+            </label>
+          </div>
 
             
         </React.Fragment>
