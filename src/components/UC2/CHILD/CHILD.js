bf6572f84219bc19d91d03a76a54c3fb1f5f451f
Dan Stronge <dnlstronge@gmail.com>
2023-02-11 13:55:22 +0000

    [x] test - create conditional div, should render /unrender onClick (clear)

--- src/components/UC2/CHILD/CHILD.js
+++ src/components/UC2/CHILD/CHILD.js
@@ -6,8 +6,8 @@ const CHILD = ({ setPropState }) => {
     const[oldest, setOldest] = useState(false)
 
     const handleOldest = (e) => {
-          oldest ? setOldest("false") : setOldest("true")
-          !oldest ? setOldest("true") : setOldest("false")
+          oldest ? setOldest(false) : setOldest(true)
+          
           
     }
     return (
@@ -15,11 +15,12 @@ const CHILD = ({ setPropState }) => {
             <div className={classes.container}>
                 <h4 className={classes.heading}>Child Elements</h4>
                 <label htmlFor="oldest_checkbox" className={classes.checkbox_label}> Oldest child born before 6-APR-2017
-                    <input onClick={handleOldest} id="oldest_checkbox" type="checkbox"></input>
+                    <input value={oldest} onClick={handleOldest} id="oldest_checkbox" type="checkbox"></input>
                 </label>
                 <label className={classes.dropdown_label} htmlFor="exempt_child"></label>
             </div>
-            <div>TEST {oldest} </div>
+            {oldest && 
+            <div>TEST BABY!!! </div>}
 
             
         </React.Fragment>
