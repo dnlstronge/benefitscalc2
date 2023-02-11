c15be1f70f42319cdf470065434e04a45007df58
Dan Stronge <dnlstronge@gmail.com>
2023-02-11 09:36:47 +0000

    [x] - fix typo

--- src/components/UC2/CHILD/CHILD.js
+++ src/components/UC2/CHILD/CHILD.js
@@ -1,12 +1,12 @@
 import React from "react";
-import classes from "./CHILD.module.classes"
+import classes from "./CHILD.module.css"
 
 const CHILD = ({ setPropState }) => {
     return (
         <React.Fragment>
             <div className={classes.container}>
                 <h4 className={classes.heading}>Child Elements</h4>
-                
+
             </div>
         </React.Fragment>
     )
