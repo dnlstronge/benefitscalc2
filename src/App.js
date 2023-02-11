c19df81d91970148bd7fbd34dd980f5ec262cc4d
Dan Stronge <dnlstronge@gmail.com>
2023-02-11 13:52:21 +0000

    [x] - general tidy and remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
-// import classes from "./App.module.css";
-import UCform from "./components/UC/UCform/UCform";
-import UCElements from "./components/UC/UCElements/UCElements";
+
+
+
 import classes from "./App.module.css"
 import React from "react";
 import Main from "./components/UC2/Main";
-import CardA from "./components/UC2/card/CardA";
-// import { useState } from "react";
 
-const UC_elements = UCElements
+
+
+
 
 function App() {
   
