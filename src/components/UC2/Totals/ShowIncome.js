ec070d3f0aad1cae0f5746f6228feba20b564766
Dan Stronge <dnlstronge@gmail.com>
2023-02-17 11:23:44 +0000

    [x] - add state for total wage [] - have use effect calc

--- src/components/UC2/Totals/ShowIncome.js
+++ src/components/UC2/Totals/ShowIncome.js
@@ -3,11 +3,16 @@ import classes from "./ShowIncome.module.css"
 
 const ShowIncome = ({wages1, wases2, unearned, other, childbenefit, workAllowance}) => {
 
-
+    const[totalWAGE, setTotalWAGE] = useState("")
     
+
     return (
         <React.Fragment>
-
+            <div className={classes.container}>
+                <label htmlFor="earnings">Total wages</label>
+                <
+                
+            </div>
 
         </React.Fragment>
     )
